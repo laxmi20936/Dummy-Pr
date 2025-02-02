b2fdbfd25599db11df79de5334b2420855d46fbf
Laxmi <waghmorelaxmi@gmail.com>
2025-02-02 23:45:45 +0530

    4th change

---
+++ 04_Function.js
@@ -0,0 +1,23 @@
+a1()
+b1()
+console.log(a)
+console.log(aka);
+
+var a = "om"
+
+
+
+function a1(){
+    var a = "AAAAA"
+    console.log(a);
+}
+
+function b1(){
+    var a = "BBB"
+    console.log(a);
+}
+
+console.log(a1, "kkkkk")
+var aka = function om(){
+    console.log("ji");
+}
