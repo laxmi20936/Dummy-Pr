338448620df73c3c6b086ebd5b4d8bfab8a53ee7
Laxmi <waghmorelaxmi@gmail.com>
2025-02-02 23:40:00 +0530

    hosting video

---
+++ 03_Hoisting.js
@@ -0,0 +1,15 @@
+
+// console.log(ses)
+console.log(a);
+aa();
+console.log(aa);
+console.log(abc());
+
+var a = 190
+function aa(){
+    console.log("Ankur");
+}
+
+var abc = () => {
+    console.log("Aka");
+}
